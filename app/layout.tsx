dd6eb37af1f2324cfda2a91873d6b9d1aacff14e
JAMI <jamisteven@gmail.com>
2026-08-26 20:14:05 +0200

    rename favicon to bypass CDN cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     icon: [
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon.ico' },
+      { url: '/favicon-new.ico' },
     ],
     apple: '/apple-touch-icon.png',
   },
